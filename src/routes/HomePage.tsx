b526e3620df32daf3878527d65e6af4b4784c758
Ebal S <ebal.seemann@ucr.ac.cr>
2025-06-05 01:18:29 -0600

    solucion errores, agregacion de section

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -3,8 +3,8 @@ import { createFileRoute } from '@tanstack/react-router'
 import Container from '../components/Container'
 
 const sectionProps = {
-  images: ['src/assets/desastres.svg', 'src/assets/caridad.svg', 'src/assets/comunidad.svg', 'src/assets/animales.svg', 'src/assets/medico.svg'],
-  images2: ['src/assets/artistico.svg', 'src/assets/deporte.svg', 'src/assets/ambiental.svg','src/assets/educacion.svg'],
+  images: ['./src/assets/desastres.svg', './src/assets/caridad.svg', './src/assets/comunidad.svg', './src/assets/animales.svg', './src/assets/medico.svg'],
+  images2: ['./src/assets/artistico.svg', './src/assets/deporte.svg', './src/assets/ambiental.svg','./src/assets/educacion.svg'],
   links: ['/Login', '/Login', '/Login', '/Login', '/'],
   links2: ['/Register', '/Register', '/', '/']
 }
