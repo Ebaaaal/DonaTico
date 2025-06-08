cfdb78f9963891a82f6654989200688dcf3c03bd
Ebal S <ebal.seemann@ucr.ac.cr>
2025-06-08 16:02:56 -0600

    NOTIFICATION PAGE complete

--- src/routes/Notifications.tsx
+++ src/routes/Notifications.tsx
@@ -10,13 +10,13 @@ export const Route = createFileRoute('/Notifications')({
 function RouteComponent() {
   return (
     <Notifications
-    prioritys={['High', 'Medium', 'Low']}
-    titles={['Donation sent', 'Donation state', 'Donation increased']}
-    texts={['Lorem ipsum dolor sit amet consectetur. Nunc imperdiet ornare aliquet enim.', 'Lorem ipsum dolor sit amet consectetur. Nunc imperdiet ornare aliquet enim.', 'Text 3']}
-    dates={['23/03/2023', '21/09/2009', '10/10/2010']}
-    images={['bellBlue.svg', 'bellRed.svg', 'bellYellow.svg']}
-    alts={['bell', 'bell', 'bell']}
+      prioritys={['High', 'Low', 'Medium', 'High', 'Low', 'Medium']}
+      titles={['Donation sent', 'Donation state', 'Donation increased', 'Donation sent', 'Donation state', 'Donation increased']}
+      texts={['Lorem ipsum dolor sit amet consectetur. Nunc imperdiet ornare aliquet enim.Lorem ipsum dolor sit amet consectetur. Nunc imperdiet ornare aliquet enim.Lorem ipsum dolor sit amet consectetur. Nunc imperdiet ornare aliquet enim.Lorem ipsum dolor sit amet consectetur. Nunc imperdiet ornare aliquet enim.', 'Lorem ipsum dolor sit amet consectetur. Nunc imperdiet ornare aliquet enim.', 'Text 3','Lorem ipsum dolor sit amet consectetur. Nunc imperdiet ornare aliquet enim.', 'Lorem ipsum dolor sit amet consectetur. Nunc imperdiet ornare aliquet enim.', 'Text 3']}
+      dates={['23/03/2023', '21/09/2009', '10/10/2010','23/03/2023', '21/09/2009', '10/10/2010']}
+      images={['bell.svg', 'bell.svg', 'bell.svg', 'bell.svg', 'bell.svg', 'bell.svg']}
+      alts={['bell', 'bell', 'bell', 'bell', 'bell', 'bell']}
 
-     />
+    />
   )
 }
