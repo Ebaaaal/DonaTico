f7a9f3d826611b87c8612ed061c32b7d449be7bd
Ebal S <ebal.seemann@ucr.ac.cr>
2025-06-15 17:58:19 -0600

    commit interface

--- src/components/pages/Register.tsx
+++ src/components/pages/Register.tsx
@@ -23,7 +23,7 @@ export default function Register(props: RegisterProps) {
             <div className="flex flex-col justify-center items-center min-h-dvh text-green-900 gap-10 font-fredoka  min-md:gap-20 my-15">
                 <h1 className="text-4xl font-bold min-md:text-6xl">{props.title}</h1>
 
-                <div className="flex flex-col gap-1 w-3/4 min-md:gap-3 min-md:text-3xl min-md:w-2/3">
+                <form className="flex flex-col gap-1 w-3/4 min-md:gap-3 min-md:text-3xl min-md:w-2/3" method='POST'>
 
                     <label className="font-semibold relative left-1/6 min-md:font-medium">{props.name}</label>
                     <input type="name" className=" border-b-3 mb-8 outline-none hover:scale-105 duration-300  min-md:mb-15" />
@@ -48,7 +48,7 @@ export default function Register(props: RegisterProps) {
                     <div className="flex gap-4 justify-center">
                         <p className="text-sm text-center min-md:text-xl hover:cursor-default ">{props.text} </p> <Link to="/Login" className="text-sm  min-md:text-xl font-semibold underline underline-offset-5 hover:scale-115 duration-400 hover:cursor-pointer">{props.link}</Link>
                     </div>
-                </div>
+                </form>
             </div>
         </section>
     )
