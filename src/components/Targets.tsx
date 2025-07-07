927e89533852df56c5e75bd585b427b9ee0edf9b
Ebal S <ebal.seemann@ucr.ac.cr>
2025-07-07 10:24:34 -0600

    frontend homepage correections

--- src/components/Targets.tsx
+++ src/components/Targets.tsx
@@ -23,16 +23,16 @@ export default function Targets(props: TargetsProps) {
                         <p className="text-xs opacity-70 "> {props.locations[number]} - location </p>
                         <p className="text-xs opacity-70 "> {props.categories[number]} </p>
                         <p className="text-xs opacity-70 "> {props.types[number]} </p>
-                        <div className="relative w-11/12 bg-gray-300 rounded-full h-3 mt-4 ">
-                            <div
-                                className="absolute bg-green-600 h-3 duration-500 rounded-full  "
+                        <div className="relative w-11/12 bg-gray-300 rounded-full h-3 mt-2 ">
+                            <div className="absolute bg-green-600 h-3 duration-500 rounded-full  "
                                 style={{
-                                    width:` ${Math.min(Math.max(Number(0), 0), 100)}%`,
+                                    width: ` ${Math.min(Math.max(Number(0), 0), 100)}%`,
                                 }}>
-                                <p className=" text-sm text-right mt-3 text-green-900 font-coiny">
-                                    {props.progress[number]} goal
-                                </p>
                             </div>
+
+                            <p className=" text-xs text-right mt-3 text-green-900 font-coiny">
+                                ₡ {props.progress[number]} goal
+                            </p>
                         </div>
                     </div>
                 </div>
